331546a40942abd6520f1dd6229aa5df8a53fe23
Almujahid Jamion <94305038+firemelon96@users.noreply.github.com>
2024-08-23 15:51:37 +0800

    1: Finish clerk authentication

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,29 @@
-const SignUp = () => {
-  return <div>Sign up Page</div>;
-};
+import { ClerkLoaded, ClerkLoading, SignUp } from '@clerk/nextjs';
+import { Loader2 } from 'lucide-react';
+import Image from 'next/image';
 
-export default SignUp;
+export default function Page() {
+  return (
+    <div className='min-h-screen grid grid-cols-1 lg:grid-cols-2'>
+      <div className='lg:flex flex-col items-center justify-center h-full px-4'>
+        <div className='text-center space-y-4 pt-16'>
+          <h1 className='font-bold text-3xl text-slate-700'>Welcome back</h1>
+          <p className='text-base text-slate-500'>
+            Log in or Create account to get back to your dashboard!
+          </p>
+        </div>
+        <div className='flex items-center justify-center mt-8'>
+          <ClerkLoaded>
+            <SignUp />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className='animate-spin text-muted-foreground' />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className='h-full hidden lg:flex bg-blue-600 items-center justify-center'>
+        <Image src='/logo.svg' width={100} height={100} alt='logo' />
+      </div>
+    </div>
+  );
+}
