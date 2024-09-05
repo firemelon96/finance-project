04fcc3c465cf386b78abe49e24ca06aaa1a12ebb
Almujahid Jamion <94305038+firemelon96@users.noreply.github.com>
2024-09-05 23:48:39 +0800

    7: Added CRUD logic to the datatable including bulk delete

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useState } from 'react';
+import { Trash } from 'lucide-react';
 import {
   ColumnDef,
   ColumnFiltersState,
@@ -22,9 +24,8 @@ import {
   TableRow,
 } from '@/components/ui/table';
 import { Button } from '@/components/ui/button';
-import { useState } from 'react';
 import { Input } from '@/components/ui/input';
-import { Trash } from 'lucide-react';
+import { useConfirm } from '@/hooks/use-confirm';
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -41,6 +42,11 @@ export function DataTable<TData, TValue>({
   onDelete,
   disabled,
 }: DataTableProps<TData, TValue>) {
+  const [ConfirmDialog, confirm] = useConfirm(
+    'Are you sure?',
+    'You are about to perform a bulk delete.'
+  );
+
   const [sorting, setSorting] = useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const [rowSelection, setRowSelection] = useState({});
@@ -63,6 +69,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
+      <ConfirmDialog />
       <div className='flex items-center py-4'>
         <Input
           placeholder={`filteer ${filterKey}`}
@@ -78,6 +85,14 @@ export function DataTable<TData, TValue>({
             size='sm'
             variant='outline'
             className='ml-auto font-normal text-xs'
+            onClick={async () => {
+              const ok = await confirm();
+
+              if (ok) {
+                onDelete(table.getFilteredSelectedRowModel().rows);
+                table.resetRowSelection();
+              }
+            }}
           >
             <Trash className='size-4 mr-2' />
             Delete ({table.getFilteredSelectedRowModel().rows.length})
--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -9,6 +9,7 @@ import { useNewAccount } from '@/features/accounts/hooks/use-new-account';
 import { AccountForm } from '@/features/accounts/components/account-form';
 import { insertAccountSchema } from '@/db/schema';
 import { z } from 'zod';
+import { useCreateAccount } from '@/features/accounts/api/use-create-account';
 
 const formSchema = insertAccountSchema.pick({ name: true });
 
@@ -17,8 +18,14 @@ type FormValues = z.input<typeof formSchema>;
 export const NewAccountSheet = () => {
   const { isOpen, onClose } = useNewAccount();
 
+  const mutation = useCreateAccount();
+
   const onSubmit = (values: FormValues) => {
-    console.log({ values });
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
   };
 
   return (
@@ -32,7 +39,7 @@ export const NewAccountSheet = () => {
         </SheetHeader>
         <AccountForm
           onSubmit={onSubmit}
-          disabled={false}
+          disabled={mutation.isPending}
           defaultValues={{ name: '' }}
         />
       </SheetContent>
