84ee46cf439966ecf87b3a0d43f07363db394670
Almujahid Jamion <94305038+firemelon96@users.noreply.github.com>
2024-09-06 14:24:43 +0800

    8: Added the categories and CRUD operations

---
+++ features/categories/components/new-category-sheet.tsx
@@ -0,0 +1,48 @@
+import { z } from 'zod';
+import { insertCategorySchema } from '@/db/schema';
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from '@/components/ui/sheet';
+
+import { useCreateCategory } from '@/features/categories/api/use-create-category';
+import { useNewCategory } from '@/features/categories/hooks/use-new-category';
+import { CategoryForm } from '@/features/categories/components/category-form';
+
+const formSchema = insertCategorySchema.pick({ name: true });
+
+type FormValues = z.input<typeof formSchema>;
+
+export const NewCategorySheet = () => {
+  const { isOpen, onClose } = useNewCategory();
+
+  const mutation = useCreateCategory();
+
+  const onSubmit = (values: FormValues) => {
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+  };
+
+  return (
+    <Sheet open={isOpen} onOpenChange={onClose}>
+      <SheetContent className='space-y-4'>
+        <SheetHeader>
+          <SheetTitle>New Category</SheetTitle>
+          <SheetDescription>Create a new category.</SheetDescription>
+        </SheetHeader>
+        <CategoryForm
+          onSubmit={onSubmit}
+          disabled={mutation.isPending}
+          defaultValues={{ name: '' }}
+        />
+      </SheetContent>
+    </Sheet>
+  );
+};
